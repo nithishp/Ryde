38e0795db4a32fb94570b80b77f48232d6cf688e
nithishp <nithishj03@gmail.com>
2024-08-20 07:55:44 +0530

    chore: Add authentication logic and token caching
    ```

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,13 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Redirect } from 'expo-router'
-
+import { useAuth } from "@clerk/clerk-expo";
 const Home = () => {
+    const { isSignedIn } = useAuth();
+
+    if (isSignedIn) {
+      return <Redirect href='/(root)/(tabs)/home' />;
+    }
   return <Redirect href='/(auth)/welcome'/>
   
 }
